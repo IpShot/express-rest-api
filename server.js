b7e9b3a24e77d4645867f0abc6ef0ea02738fdcd
IpShot <zhernosek.roman@gmail.com>
2020-09-03 15:49:23 +0500

    add dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -6,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`)
 })
