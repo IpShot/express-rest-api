a19e3b90a774201615c304f23704e722b61c023f
IpShot <zhernosek.roman@gmail.com>
2020-09-07 17:12:25 +0500

    add promise unhandled rejection handler

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const logger = require('./logger')
 const app = require('./app')
 const db = require('./db')
 
+// Register handler of all missed promises rejections and throw them to logger
+process.on('unhandledRejection', (reason) => {
+  throw reason
+})
+
 async function start() {
   try {
     await db.connect()
     logger.info('MongoDB connected successfully')
   } catch (e) {
     logger.error(`MongoDB connection error: ${e}`)
-    process.exit(-1)
+    process.exit(1)
   }
+
   app.listen(process.env.PORT, () => {
     logger.info(`Server started on port ${process.env.PORT}`)
   })
